a2501d44989e00cea01cb974bd4abfcd86feb090
0Erik1 <erikpinheiroalves@gmail.com>
2026-08-17 20:41:24 -0300

    feat: delete exercise

--- src/routes/exerciseRoutes.ts
+++ src/routes/exerciseRoutes.ts
@@ -1,10 +1,12 @@
 import {Router} from "express";
 import getExercise from "../controllers/exercises/getExercise.js";
 import getExerciseById from "../controllers/exercises/getExerciseById.js";
+import deletExercise from "../controllers/exercises/deletExercise.js";
 
 const router = Router();
 
 router.get("/", getExercise);
 router.get("/:id", getExerciseById);
+router.delete("/",deletExercise);
 
 export default router;
