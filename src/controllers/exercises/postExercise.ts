749aa6dc59263aa1d404677bf3799c9f0551fea7
0Erik1 <erikpinheiroalves@gmail.com>
2026-08-18 19:57:59 -0300

    feat: post router

---
+++ src/controllers/exercises/postExercise.ts
@@ -0,0 +1,35 @@
+import {prisma} from "../../prisma.js";
+import type { Response, Request } from "express";
+
+export async function postExercise(req:Request, res:Response) {
+    const {id,name,force,level,category,equipment,primaryMuscles,mechanic,secondaryMuscles,instructions} = req.body;
+    
+    if(id && name){
+        try{
+            await prisma.exercise.create({
+                data: 
+                    {
+                    id:id,
+                    name:name,
+                    force:force,
+                    level:level,
+                    category:category,
+                    equipment:equipment,
+                    primaryMuscles:primaryMuscles,
+                    mechanic:mechanic,
+                    secondaryMuscles:secondaryMuscles,
+                    instructions:instructions
+                }
+            });
+            res.status(201).send();
+        }
+        catch(error){
+            //fazer o tratamento de erro depois
+            console.log(error);
+            res.status(500).json({
+                message: "Erro ao criar exercício"
+            });
+        }
+    }
+    
+}
--- src/routes/exerciseRoutes.ts
+++ src/routes/exerciseRoutes.ts
@@ -2,12 +2,13 @@ import {Router} from "express";
 import getExercise from "../controllers/exercises/getExercise.js";
 import getExerciseById from "../controllers/exercises/getExerciseById.js";
 import deleteExercise from "../controllers/exercises/deleteExercise.js";
+import { postExercise } from "../controllers/exercises/postExercise.js";
 
 const router = Router();
 
 router.get("/", getExercise);
 router.get("/:id", getExerciseById);
 router.delete("/",deleteExercise);
-
+router.post("/", postExercise);
 
 export default router;
