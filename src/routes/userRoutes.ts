652ce7b6af1e6a3dde988d7dec9d731c2b7d757a
0Erik1 <erikpinheiroalves@gmail.com>
2026-08-23 13:23:04 -0300

    feat: add new routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import postUser from "../controllers/user/postUser.js";
+import getUser from "../controllers/user/getUser.js";
+import deleteUser from "../controllers/user/deleteUser.js";
+import putUser from "../controllers/user/putUser.js";
 
 const router = Router();
 
 router.post("/",postUser);
+router.delete("/:id",deleteUser);
+router.get("/:id",getUser);
+router.put("/:id",putUser);
 
 export default router;
