d8dfa6d88b64eecb979cde145e8bf28d2890f3cf
0Erik <erikpinheiroalves@gmail.com>
2026-09-01 21:18:17 -0300

    feat: hash user password on creation

--- src/controllers/user/postUser.ts
+++ src/controllers/user/postUser.ts
@@ -1,16 +1,26 @@
 import {prisma} from "../../prisma.js";
 import type {Request, Response} from "express";
+import bcrypt from "bcrypt";
 
 export default async function postUser(req:Request, res:Response){
     const {name,email,password} = req.body;
 
     try{
+        const userExists = await prisma.user.findUnique({
+            where: { email }
+        });
+        if (userExists) {
+            return res.status(400).json({ message: "E-mail já cadastrado." });
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
         if(name && email && password){
             await prisma.user.create({
                 data:{
                     name,
                     email,
-                    password
+                    password: passwordHash
                 }
             })
             res.status(201).send();
